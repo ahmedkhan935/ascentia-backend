48e50bba53789a306a5b2c054e6b5793080b1319
Sohaib-Ahmed869 <i211114@nu.edu.pk>
2025-01-25 18:36:13 +0500

    ok

--- models/Room.js
+++ models/Room.js
@@ -1,38 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const RoomSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  bookings: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      date: {
+        type: Date,
+        required: true,
+      },
+      startTime: {
+        type: String,
+        required: true,
+      },
+      endTime: {
+        type: String,
+        required: true,
+      },
+      class: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Class",
+      },
     },
-    description:{
-        type:String,
-        required:true
-    },
-    bookings:[{
-        user:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:'User'
-        },
-        date:{
-            type:Date,
-            required:true
-        },
-        startTime:{
-            type:String,
-            required:true
-        },
-        endTime:{
-            type:String,
-            required:true
-        },
-        class:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:'Class'
-        }
-    }],
-    createdAt:{
-        type:Date,
-        default:Date.now
-    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
